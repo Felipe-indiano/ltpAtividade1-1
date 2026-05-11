0fb949873078acc8ff6b49127de3aaded7f6077f
Carretafuracao2000 <joao.santos71@estudante.ifms.edu.br>
2026-05-11 22:45:24 +0000

    criando o cadastro de livros

--- main.js
+++ main.js
@@ -17,4 +17,13 @@ while(acao == 0){
     if(acao == 0){
         console.log(`Fechando prograna`)
     }
+    if(acao == 1){
+        let titulo = prompt(`Qual o título do livro: `);
+        let autor = prompt(`Qual o nome do autor: `)
+        let genero = prompt(`Qual o gênero do livro: `)
+        let anoPub = parseInt(prompt(`Qual o  ano de laçamento do livro: `))
+        let numPaginas = parseInt(prompt(`Qual o número de página do livro: `))
+
+        livros.push(new Livro(titulo, autor, genero, anoPub, numPaginas));
+    }
 }
